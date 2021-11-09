24bf197dfc096a53bcc55419a073efd04791f935
JAMES GLASS <james.glass1155@gmail.com>
2021-11-08 16:48:48 -0800

    routing enabled

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './header.css';
 
 export default function Header() {
@@ -6,8 +7,12 @@ export default function Header() {
     <div className="header">
       <h1>Flash Cards</h1>
       <div className="buttons">
-        <button>review</button>
-        <button>add</button>
+        <button type="button" className="header-button">
+          <Link to="">review</Link>
+        </button>
+        <button type="button" className="header-button">
+          <Link to="add">add</Link>
+        </button>
       </div>
     </div>
   );
--- src/components/header/header.test.js
+++ src/components/header/header.test.js
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import TestRenderer from 'react-test-renderer';
 import Header from './header';
 
@@ -6,13 +7,19 @@ describe('Header', () => {
   test('non-shallow render', () => {
     // eslint-disable-next-line new-cap
     const element = new TestRenderer.create(
-      <Header />,
+      <Router>
+        <Header />
+      </Router>,
     );
     expect(element.toJSON()).toMatchSnapshot();
   });
 
   it('says Flash Cards', () => {
-    const header = render(<Header />);
+    const header = render(
+      <Router>
+        <Header />
+      </Router>,
+    );
     const msg = header.getByText('Flash Cards');
     expect(msg).toBeInTheDocument(1);
   });
