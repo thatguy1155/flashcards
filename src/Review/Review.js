0e5a5a4cad7af1842dcce11849c86419d7f43820
JAMES GLASS <james.glass1155@gmail.com>
2021-11-11 17:37:34 -0800

    nav buttons included

--- src/Review/Review.js
+++ src/Review/Review.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Card from './components/card/card';
 import './Review.css';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 export default function Review() {
   return (
     <div className="review-page">
-      <Card />
+      <div className="nav-buttons">
+        <FaArrowLeft />
+        <FaArrowRight />
+      </div>
+      <Card english="hello" korean="안녕" language="eng" />
     </div>
   );
 }
